0b2c3141f85812b8acd6f90a205c5b2e756cf3b1
Chris Fernando <chris.t.fernando@gmail.com>
2022-08-07 20:30:07 +1000

    renamed and fixed package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="notification_service",
+    version="0.1",
+    description="Interface and concretes for sending notifications",
+    author="Chris Fernando",
+    author_email="chris.t.fernando@gmail.com",
+    url="https://github.com/chris-t-fernando/notification-service",
+    packages=["notification_service"],
+    install_requires=["slack_sdk", "git+https://github.com/Wyattjoh/pushover"],
+)
